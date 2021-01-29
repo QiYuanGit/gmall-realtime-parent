59d48a796b9bd321b1868b72d71002d15f1e2919
felix <fxf_java@163.com>
2021-01-29 16:42:19 +0800

    gmall0820-logger-create

--- gmall0820-logger/src/main/java/com/atguigu/gmall/controller/LoggerController.java
+++ gmall0820-logger/src/main/java/com/atguigu/gmall/controller/LoggerController.java
@@ -1,9 +1,16 @@
 package com.atguigu.gmall.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Properties;
 
 /**
  * Author: Felix
@@ -14,12 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class LoggerController {
+
+    //KafkaTemplate是Spring提供对kafka操作的类
+
+    @Autowired //注入
+    KafkaTemplate kafkaTemplate;
+
     @RequestMapping("/applog")
     public String logger(@RequestParam("param") String jsonLog){
         //1.打印输出到控制台
         //System.out.println(jsonLog);
         //2.落盘   借助记录日志的第三方框架 log4j [logback]
         log.info(jsonLog);
+        //3.将生成的日主发送到kafka对应的主题中
+       kafkaTemplate.send("ods_base_log",jsonLog);
+
         return "success";
     }
 }
